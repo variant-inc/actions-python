bee8c74f6bf11ca9ad2056ede313de791ea6e45e
Samir <salyateem13@gmail.com>
2021-09-17 12:27:37 -0400

    add xml output for coverage

--- scripts/coverage_scan.sh
+++ scripts/coverage_scan.sh
@@ -2,19 +2,33 @@
 
 set -euo
 
+echo "---Start: pytest test"
+REQUIREMENTS_TXT="requirements.txt"
+echo "---pip install"
+if [ -f "$REQUIREMENTS_TXT" ]; then
+  # shellcheck disable=SC1091
+  python -m venv env &&\
+	  source env/bin/activate &&\
+    python -m pip install --requirement "$REQUIREMENTS_TXT"
+else
+  python -m pip install --upgrade pipenv wheel
+  pipenv install --dev
+fi
+
+python -m pip install pytest wheel coverage &&\
+	coverage run -m pytest test &&\
+	coverage xml -i -o coverage.xml
+echo "---End: pytest test"
+
 pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 export PULL_REQUEST_KEY=$pull_number
 
-echo "running lazy sonar tests."
-
-export OUTPUTDIR="coverage"
-mkdir -p "$OUTPUTDIR"
+echo "------Sonar tests."
 
 sonar_args="-Dsonar.host.url=https://sonarcloud.io \
             -Dsonar.login=$SONAR_TOKEN \
-            -Dsonar.scm.disabled=true \
-            -Dsonar.python.coverage.reportPaths=$OUTPUTDIR/coverage.xml \
-            -Dsonar.scm.revision=$GITHUB_SHA"
+            -Dsonar.scm.revision=$GITHUB_SHA \
+            -Dsonar.python.coverage.reportPaths=coverage.xml"
 
 if [ "$PULL_REQUEST_KEY" = null ]; then
   echo "Sonar run when pull request key is null."
