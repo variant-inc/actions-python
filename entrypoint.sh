dcc52e5900aa4c909d3930d221e9d714cf23eb6d
Naveen <naveenp@drivevariant.com>
2022-01-24 15:18:13 -0500

    formatting

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,7 +34,7 @@ echo "Print Branch name: $BRANCH_NAME"
 echo "---End: Setting Prerequisites"
 
 echo "Start: Enable sonar"
-  ./actions-collection/scripts/enable_sonar.sh
+./actions-collection/scripts/enable_sonar.sh
 echo "End: Enable sonar"
 
 echo "---Start: Sonar Scan"
