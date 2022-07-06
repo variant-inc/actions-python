6d45ae376113196837a5dbd3944ca32d012f0b90
Sathwik <99672063+sdoguparti@users.noreply.github.com>
2022-07-06 13:17:39 -0500

    CLOUD-1855: testing workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ echo "Current directory: $(pwd)"
 pip install --upgrade --no-cache-dir wheel pip
 
 echo "Cloning into actions-collection..."
-git clone -b v1 https://github.com/variant-inc/actions-collection.git ./actions-collection
+git clone -b f/CLOUD-1855 https://github.com/variant-inc/actions-collection.git ./actions-collection
 
 echo "---Start: Pretest script"
 chmod +x ./actions-collection/scripts/pre_test.sh
